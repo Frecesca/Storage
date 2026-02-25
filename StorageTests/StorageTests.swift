9f777139d19f86eb7895c9ffc1f6420af616261d
Frecesca Wang <meixuw2@uw.edu>
2026-02-25 15:45:15 +0800

    Initial Commit

---
+++ StorageTests/StorageTests.swift
@@ -0,0 +1,17 @@
+//
+//  StorageTests.swift
+//  StorageTests
+//
+//  Created by shao on 2026/2/25.
+//
+
+import Testing
+@testable import Storage
+
+struct StorageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
